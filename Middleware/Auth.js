a239aafed5c2871bc7184242b4d79b2fa7ecfb22
softdev-nic <softtechon.inc@gmail.com>
2026-08-19 14:23:32 +0530

    auth misplace

--- Middleware/Auth.js
+++ Middleware/Auth.js
@@ -1,22 +1,19 @@
- const privateAPI = axios.create({
-  baseURL: BASEURL,
-});
+const jwt = require("jsonwebtoken");
+const User = require("../Model/Users");
 
-const publicAPI = axios.create({
-  baseURL: BASEURL,
-});
+const authMiddleware = async (req, res, next) => {
+  const token = req.header("Authorization")
+ if(!token){
+    return res.status(401).json({ message: "No token, authorization denied" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.userId).select("-password");
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({ message: "Token is not valid" });
+  }
+};
 
-publicAPI.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("auth-token");
-
-    if (token) {
-      config.headers.Authorization = token;
-    }
-
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-export { privateAPI, publicAPI };
+module.exports = authMiddleware;    
